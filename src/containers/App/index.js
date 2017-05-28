cd06dfbeb3ff066e61752080480bea8ef82907a1
Slava <slava@MacBook-Pro-Slava.local>
2017-05-28 03:46:46 +0300

    update folder structure

---
+++ src/containers/App/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import Header from './../../components/Header/index.js'
+
+
+class App extends Component {
+  render() {
+    return (
+      <Header />      
+    );
+  }
+}
+
+export default App;
